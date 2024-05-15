721e3d5ddaeb48d76f68ed6fe4ada1c4f898bdc2
sdmiplurmila <asmi.urmila22@gmail.com>
2024-05-15 10:26:07 +0530

    created some section with responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+
+jQuery("#carousel").owlCarousel({
+  autoplay: true,
+  rewind: false, /* use rewind if you don't want loop */
+  margin: 20,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 1
+    },
+
+    1024: {
+      items: 2
+    },
+
+    1366: {
+      items: 2
+    }
+  }
+});
+
+
+
+
+
 // Range Counting
 var rangeInput = document.getElementById('rangeInput');
 // Get current value display element
@@ -8,3 +45,5 @@ rangeInput.addEventListener('input', function() {
   // Update current value display
   currentValue.innerText = this.value + '%';
 });
+
+// Carousel
